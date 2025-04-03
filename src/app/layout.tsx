e16836b852a3b4c50e4ce06962738a779bd193e7
Nelson Venegas <nelson.venegasg@gmail.com>
2025-04-02 22:42:03 -0300

    fix: Fixing responsive problems

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
 
-const poppins = Poppins({ weight: ['400', '700', '800'], subsets: ['latin'] });
+const poppins = Poppins({ weight: ['400', '700', '800'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: 'Age Calculator',
-	description: 'Calculate the time you have been in this world'
-};
+  title: 'Age Calculator',
+  description: 'Calculate the time you have been in this world'
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang='en'>
-			<body className={`${poppins.className} transition-all duration-150 ease-linear h-screen `}>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang='en'>
+      <body className={`${poppins.className} transition-all duration-150 ease-linear h-screen`}>
+        {children}
+      </body>
+    </html>
+  )
 }
